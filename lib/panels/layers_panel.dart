89267bf265aa49917a8b36412e4cc647a05fafeb
JP Duplessis <jp@vteam.com>
2025-01-25 22:25:37 -0500

    toogle layer visibility from mini Layer list

--- lib/panels/layers_panel.dart
+++ lib/panels/layers_panel.dart
@@ -45,94 +45,107 @@ class LayersPanel extends StatelessWidget {
               ),
             if (appModel.isSidePanelExpanded) Spacer(),
             IconButton(
-              icon: Icon(appModel.isSidePanelExpanded
-                  ? Icons.chevron_left
-                  : Icons.chevron_right),
+              icon: Icon(
+                appModel.isSidePanelExpanded
+                    ? Icons.chevron_left
+                    : Icons.chevron_right,
+              ),
               onPressed: () {
                 appModel.isSidePanelExpanded = !appModel.isSidePanelExpanded;
               },
             ),
           ],
         ),
-        if (appModel.isSidePanelExpanded)
-          Expanded(
-            child: Consumer<AppModel>(
-              builder: (context, appModel, child) {
-                return ReorderableListView.builder(
-                  itemCount: appModel.layers.length,
-                  buildDefaultDragHandles: false,
-                  onReorder: (oldIndex, newIndex) {
-                    if (newIndex > oldIndex) {
-                      newIndex -= 1;
-                    }
-                    final Layer layer = appModel.layers.get(oldIndex);
-                    appModel.layers.remove(oldIndex);
-                    appModel.layers.insert(newIndex, layer);
-                    if (selectedLayerIndex == oldIndex) {
-                      onSelectLayer(newIndex);
-                    }
-                  },
-                  itemBuilder: (context, index) {
-                    final Layer layer = appModel.layers.get(index);
-                    final bool isSelected = index == selectedLayerIndex;
-                    return ReorderableDragStartListener(
-                      key: Key('$index'),
-                      index: index,
-                      child: GestureDetector(
-                        onTap: () => onSelectLayer(index),
-                        child: layerRow(
-                          context,
-                          isSelected,
-                          index,
-                          layer,
-                          appModel.layers.length > 1,
-                        ),
+
+        Expanded(
+          child: Consumer<AppModel>(
+            builder: (context, appModel, child) {
+              return ReorderableListView.builder(
+                itemCount: appModel.layers.length,
+                buildDefaultDragHandles: false,
+                onReorder: (oldIndex, newIndex) {
+                  if (newIndex > oldIndex) {
+                    newIndex -= 1;
+                  }
+                  final Layer layer = appModel.layers.get(oldIndex);
+                  appModel.layers.remove(oldIndex);
+                  appModel.layers.insert(newIndex, layer);
+                  if (selectedLayerIndex == oldIndex) {
+                    onSelectLayer(newIndex);
+                  }
+                },
+                itemBuilder: (context, index) {
+                  final Layer layer = appModel.layers.get(index);
+                  final bool isSelected = index == selectedLayerIndex;
+                  return ReorderableDragStartListener(
+                    key: Key('$index'),
+                    index: index,
+                    child: GestureDetector(
+                      onTap: () => onSelectLayer(index),
+                      onDoubleTap: () => onToggleViewLayer(index),
+                      child: layerSelector(
+                        context,
+                        isSelected,
+                        index,
+                        layer,
+                        appModel.layers.length > 1,
+                        appModel.isSidePanelExpanded,
                       ),
-                    );
-                  },
-                );
-              },
-            ),
+                    ),
+                  );
+                },
+              );
+            },
           ),
+        ),
       ],
     );
   }
 
-  Widget layerRow(
+  Widget layerSelector(
     final BuildContext context,
     final bool isSelected,
     final int index,
     final Layer layer,
     final bool showDelete,
+    final bool isExpanded,
   ) {
     return Container(
       key: ValueKey(index),
       margin: EdgeInsets.all(4),
       padding: EdgeInsets.all(8),
       decoration: BoxDecoration(
+        color: isExpanded
+            ? null
+            : layer.isVisible
+                ? null
+                : Colors.grey,
         border: Border.all(
           color: isSelected ? Colors.blue : Colors.grey.shade300,
           width: 3,
         ),
         borderRadius: BorderRadius.circular(4),
       ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Text(layer.name),
-          ),
-          IconButton(
-            icon:
-                Icon(layer.isVisible ? Icons.visibility : Icons.visibility_off),
-            onPressed: () => onToggleViewLayer(index),
-          ),
-          if (showDelete)
-            IconButton(
-              icon: Icon(Icons.delete_outline),
-              onPressed: () => onRemoveLayer(index),
-            ),
-        ],
-      ),
+      child: isExpanded
+          ? Row(
+              children: [
+                Expanded(
+                  child: Text(layer.name),
+                ),
+                IconButton(
+                  icon: Icon(
+                    layer.isVisible ? Icons.visibility : Icons.visibility_off,
+                  ),
+                  onPressed: () => onToggleViewLayer(index),
+                ),
+                if (showDelete)
+                  IconButton(
+                    icon: Icon(Icons.delete_outline),
+                    onPressed: () => onRemoveLayer(index),
+                  ),
+              ],
+            )
+          : Text('$index', style: TextStyle(fontSize: 10)),
     );
   }
 }
