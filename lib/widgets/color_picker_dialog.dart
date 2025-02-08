05075539f9c93ade9e1517fce305ada811cda103
JP Duplessis <jp@vteam.com>
2025-02-08 11:31:49 +0000

    Hex Color in ColorPickerDialog

--- lib/widgets/color_picker_dialog.dart
+++ lib/widgets/color_picker_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fpaint/helpers/color_helper.dart';
 import 'package:fpaint/models/app_model.dart';
 import 'package:fpaint/widgets/color_preview.dart';
@@ -21,11 +22,20 @@ class ColorPickerDialog extends StatefulWidget {
 
 class _ColorPickerDialogState extends State<ColorPickerDialog> {
   late Color _currentColor;
+  late TextEditingController _hexController;
 
   @override
   void initState() {
     super.initState();
     _currentColor = widget.color;
+    _hexController =
+        TextEditingController(text: colorToHexString(_currentColor));
+  }
+
+  @override
+  void dispose() {
+    _hexController.dispose();
+    super.dispose();
   }
 
   @override
@@ -39,6 +49,8 @@ class _ColorPickerDialogState extends State<ColorPickerDialog> {
             mainAxisSize: MainAxisSize.min,
             spacing: 20,
             children: [
+              //----------------------------
+              // Color preview and selection sliders
               Row(
                 spacing: 10,
                 children: [
@@ -62,12 +74,14 @@ class _ColorPickerDialogState extends State<ColorPickerDialog> {
                       onColorChanged: (color) {
                         setState(() {
                           _currentColor = color;
+                          _hexController.text = colorToHexString(color);
                         });
                       },
                     ),
                   ),
                 ],
               ),
+
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
@@ -83,6 +97,7 @@ class _ColorPickerDialogState extends State<ColorPickerDialog> {
                       onTap: () {
                         setState(() {
                           _currentColor = color;
+                          _hexController.text = colorToHexString(color);
                         });
                       },
                       child: Container(
@@ -100,6 +115,9 @@ class _ColorPickerDialogState extends State<ColorPickerDialog> {
                     ),
                 ],
               ),
+
+              //----------------------------
+              // Top colors used in the image
               TopColors(
                 colorUsages: appModel.topColors,
                 onRefresh: () {
@@ -107,6 +125,68 @@ class _ColorPickerDialogState extends State<ColorPickerDialog> {
                     appModel.evaluatTopColor();
                   });
                 },
+              ),
+
+              //----------------------------
+              // Hex value edit copy/paste
+              SizedBox(
+                width: 200,
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.paste),
+                      onPressed: () async {
+                        final data = await Clipboard.getData('text/plain');
+
+                        try {
+                          final color = getColorFromString(
+                            data?.text! as String,
+                          ); // #FF00FF00
+                          setState(() {
+                            _currentColor = color;
+                            _hexController.text = colorToHexString(color);
+                          });
+                        } catch (e) {
+                          // Invalid hex color format
+                        }
+                      },
+                    ),
+                    Expanded(
+                      child: TextField(
+                        controller: _hexController,
+                        decoration: const InputDecoration(
+                          labelText: 'Hex Color',
+                          border: OutlineInputBorder(),
+                        ),
+                        onChanged: (value) {
+                          try {
+                            final color = getColorFromString(value);
+                            setState(() {
+                              _currentColor = color;
+                            });
+                          } catch (e) {
+                            // Invalid hex color format
+                          }
+                        },
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.copy),
+                      onPressed: () {
+                        Clipboard.setData(
+                          ClipboardData(
+                            text: colorToHexString(_currentColor),
+                          ),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Hex Color copied to clipboard'),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
